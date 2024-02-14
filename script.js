f704135e756f5e862da9bc098b028cb4944cf3fc
PrinYr <gapth33@gmail.com>
2024-02-14 13:16:24 +0700

    add high score

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
+let highscore = 0;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
@@ -16,6 +18,11 @@ document.querySelector(".check").addEventListener("click", function () {
 
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
 
     // num too high
   } else if (guess > secretNumber) {
